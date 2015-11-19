f0af7e513010c23a9c6438579c80e2669dae37f3
seth wolfwood <seth@sethish.com>
2015-11-19 15:54:15 -0500

    Fixes volume widget buttons

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -191,9 +191,9 @@ volpct:buttons(awful.util.table.join(
      awful.button({ }, 1,
      function() awful.util.spawn_with_shell("amixer -q set Master toggle") end),
      awful.button({ }, 4,
-     function() awful.util.spawn_with_shell("amixer -q set Master 3+% unmute") end),
+     function() awful.util.spawn_with_shell("amixer -q set Master 3%+ unmute") end),
      awful.button({ }, 5,
-     function() awful.util.spawn_with_shell("amixer -q set Master 3-% unmute") end)
+     function() awful.util.spawn_with_shell("amixer -q set Master 3%- unmute") end)
             ))
  -- End Volume }}}
 
