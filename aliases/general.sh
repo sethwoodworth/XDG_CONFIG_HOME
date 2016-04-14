4711dd260932a13fb1b2c5ddccebe6f9c056a0fc
Seth Woodworth <seth@sethish.com>
2016-04-13 23:42:18 -0400

    updating aliases

--- aliases/general.sh
+++ aliases/general.sh
@@ -1,3 +1,5 @@
+alias g-o='gnome-open'
+alias ln='ln -v' # unlike other unix commands, this is silent on success or failure
 alias pubkey='cat ~/.ssh/id_rsa.pub | xclip -selection clipboard'
 alias tmux='tmux -2'
 alias tx='tmux -2 attach'
