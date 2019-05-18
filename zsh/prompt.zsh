0ef57cead2cedfce9f927ce444641a6d9bc03668
Seth Woodworth <seth@sethish.com>
2019-05-17 21:54:10 -0400

    Fixes __git_ps1 implementation

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,9 +1,9 @@
-setopt PROMPT_SUBST
-setopt PROMPT_PERCENT
-setopt PROMPT_BANG
+setopt prompt_subst
+setopt prompt_percent
+setopt prompt_bang
 source /usr/lib/git-core/git-sh-prompt
 export GIT_PS1_SHOWDIRTYSTATE=1
-PROMPT="
-%B%F{red}┍━━━⎧⦇ %F{white}!s:%?%F{red} ⦈━(%F{white}%j%F{red})━[ %F{white}%~:%F{yellow}$(__git_ps1 "%s")%F{red} ]
+precmd () {
+  PROMPT="%B%F{red}┍━━━⎧⦇ %F{white}!s:%?%F{red} ⦈━(%F{white}%j%F{red})━[ %F{white}%~:%F{yellow}$(__git_ps1 "%s")%F{red} ]
 ╘═══⎩%b%f "
-
+}
